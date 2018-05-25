fdcdef7262143bbf413aefe53cde0e8596ce6d55
jake-bickle <bickle.jake@gmail.com>
2018-05-24 17:52:08 -0700

    Complete addNextLimb()

--- debug/board.cpp
+++ debug/board.cpp
@@ -6,7 +6,29 @@ void Board::addNextLimb(){
 
     switch(limbsAttached){
         case(1):
-
+            replaceCharOnBoard(TORSO, ROPE_COORD);
+            return;
+        case(2):
+            replaceCharOnBoard(HEAD, HEAD_COORD);
+            return;
+        case(3):
+            replaceCharOnBoard(TORSO, TORSO_COORD);
+            return;
+        case(4):
+            replaceCharOnBoard(ARM, LEFT_ARM_COORD);
+            return;
+        case(5):
+            replaceCharOnBoard(ARM, RIGHT_ARM_COORD);
+            return;
+        case(6):
+            replaceCharOnBoard(LEFT_LEG, LEFT_LEG_COORD);
+            return;
+        case(7):
+            replaceCharOnBoard(RIGHT_LEG, RIGHT_LEG_COORD);
+            return;
+        default:
+            return;
+            // TODO: Throw error
     }
 }
 
