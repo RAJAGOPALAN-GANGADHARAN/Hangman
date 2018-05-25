ce1212a1840d9b811c25a39f3ea9313574c0b0a7
jake-bickle <bickle.jake@gmail.com>
2018-05-24 17:40:27 -0700

    hangmanUI class is now board class

--- debug/hangmanUI.h
+++ debug/hangmanUI.h
@@ -2,60 +2,14 @@
 #define HANGMANUI_H
 #include <array>
 
-struct Point{
-    int x;
-    int y;
-};
+
 
 class HangmanUI{
 
 public:
 
-    //TODO: Add as member of Game
-    //TODO: Rename these functions
-    void addLimb();  // Adds limb to hangman output stream and increments
-
-    // Operator overload
-    friend std::ostream& operator <<(std::ostream& os,
-                                    const HangmanUI& hgMnUI);
-
 private:
     
-    // Private methods
-    void replaceCharOnBoard(char ch, Point coord); 
-    
-    // Attributes
-    unsigned int limbsAttached = 0;
-
-    const Point HEAD_COORD = {3, 6};
-    const char HEAD = 'o';
-
-    const Point ROPE_COORD = {2, 6}; 
-    const Point TORSO_COORD = {4, 6};
-    const char TORSO = '|';
-
-    const Point LEFT_ARM_COORD = {4, 5};
-    const Point RIGHT_ARM_COORD = {4, 7};
-    const char ARM = '-';
-
-    const Point LEFT_LEG_COORD = {5, 5};
-    const char LEFT_LEG = '/';
-
-    const Point RIGHT_LEG_COORD = {5, 7};
-    const char RIGHT_LEG = '\\';
-
-    // The top left is {0, 0}
-    char board[9][13] = 
-    {{' ', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', ' '},
-     {' ', '|', '.', '_', '_', '_', '_', '_', '_', '_', '.', '|', ' '},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '},
-     {'_', '|', '|', '_', '_', '_', '_', '_', '_', '_', '|', '|', '_'},
-     {' ', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|', ' '}};
-
-};
+    };
 
 #endif
