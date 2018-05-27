05ebdb275313641909905e605f750ec838660003
jake-bickle <bickle.jake@gmail.com>
2018-05-26 19:32:08 -0700

    Begin writing hangman class

--- hangman.h
+++ hangman.h
@@ -2,15 +2,18 @@
 #define HANGMAN_H
 
 #include "randomWord.h"
-#include "hangman.h"
+#include "hangmanUI.h"
 
 class Hangman{
 public:
+    void incrementWrongAttempts();
+    const bool& isGameLost();
 
 private:
-
-    unsigned int const totalWrongAttempts = 7;
+    unsigned int const TOTAL_WRONG_ATTEMPTS = 7;
     unsigned int wrongAttempts = 0;
+    bool playerLost = false;
+
     RandomWord targetPhrase;
     HangmanUI ui;
 };
