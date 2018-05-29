546d4222a312d2b6497508e26b85e44ce2b7101b
jake-bickle <bickle.jake@gmail.com>
2018-05-29 00:29:06 -0700

    Merged letterBox and hangmanUI class together. LetterBox doesn't need to be an object.

--- hangmanUI.cpp
+++ hangmanUI.cpp
@@ -1,7 +1,39 @@
 #include "hangmanUI.h"
 
-void HangmanUI::display(){
-    std::cout << *this;
+//void HangmanUI::display(){
+    //std::cout << *this;
+//}
+
+void HangmanUI::displayLetterBox(const char* letters){
+    unsigned int size;
+    size = sizeof(letters) / sizeof(letters[0]);
+
+    displayLetterBox(letters, size);
+}
+
+void HangmanUI::displayLetterBox(const char* letters,
+                                 const unsigned int &letterAmount){
+    // Each letter takes up 3 cols of space, the letter itself, and a space on both sides
+    unsigned int sizeBetweenBorders = letterAmount * 3 + 1;
+
+    // Upper half
+    std::cout << '*' << std::setw(sizeBetweenBorders) << std::setfill('-') 
+              << '*' << std::endl
+              << '|' << std::setw(sizeBetweenBorders) << std::setfill(' ') 
+              << '|' << std::endl
+              << '|';
+    // Letters
+    for (unsigned int i = 0; i < letterAmount; i++){
+        std::cout << ' ' << letters[i] << ' ';
+    }
+
+    std::cout << '|' << std::endl;
+
+    // Lower half
+    std::cout << '|' << std::setw(sizeBetweenBorders) << std::setfill(' ') 
+              << '|' << std::endl
+              << '*' << std::setw(sizeBetweenBorders) << std::setfill('-') 
+              << '*';
 }
 
 std::ostream& operator<<(std::ostream &os, const HangmanUI &ui){
--- hangmanUI.h
+++ hangmanUI.h
@@ -1,12 +1,23 @@
+/*
+*---------*
+|         |
+| H  S  T |       <- Example of letter box
+|         |
+|---------|
+*/
+
 #ifndef HANGMANUI_H
 #define HANGMANUI_H
 
 #include "board.h"
+#include <iomanip>
 
 class HangmanUI{
 
 public:
-    void display();
+    //void display();
+    void displayLetterBox(const char* letters);
+    void displayLetterBox(const char* letters, const unsigned int &letterAmount);
 
     friend std::ostream& operator<<(std::ostream &os, const HangmanUI &ui);
 
