524cc0eed9b56cf497c4aabb4b70ed8674f0d304
jake-bickle <bickle.jake@gmail.com>
2018-05-28 23:46:37 -0700

    Feat: Add LetterBox class

---
+++ letterBox.h
@@ -0,0 +1,15 @@
+#ifndef LETTERBOX_H
+#define LETTERBOX_H
+
+#include <iostream>
+#include <iomanip>
+
+class LetterBox{
+public:
+
+    //TODO: Determine if size works
+    void displayLetters(const char* letters);
+    void displayLetters(const char* letters, const unsigned int &letterAmount);
+};
+
+#endif
