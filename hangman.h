9a071478a8d3663ebd317ac6f86a4cd42327e864
jake-bickle <bickle.jake@gmail.com>
2018-05-30 11:17:15 -0700

    Feat: Add attributes and members to Hangman class Change: lettersGuessedIncorrectly and lettersGuessedCorrectly are std::set instead std::vector

--- hangman.h
+++ hangman.h
@@ -4,21 +4,32 @@
 #include "randomWord.h"
 #include "hangmanUI.h"
 #include <string> 
+#include <set>
 
 class Hangman{
 public:
     Hangman();
+    Hangman(std::string targetPhrase);
+    
+    void guessLetter(const char& letter);
 
-    void incrementWrongAttempts();
+    void incrWrongAttempts();
     const bool& isGameLost();
+    const bool& isGameWon();
+
+    std::set<char>& getIncorrectLetters();
+    std::set<char>& getCorrectLetters();
+
+    void updateUI();
+    void displayUI();
 
 private:
     unsigned int const TOTAL_WRONG_ATTEMPTS = 7;
     unsigned int wrongAttempts = 0;
-    bool playerLost = false;
-    std::vector<char> incorrectLetters;
-
     std::string targetPhrase;
+    //TODO: UI will point to lettersGuessedIncorrectly and lettersGussedCorrectly 
+    std::set<char> lettersGuessedIncorrectly;
+    std::set<char> lettersGuessedCorrectly;
     HangmanUI ui;
 };
 
