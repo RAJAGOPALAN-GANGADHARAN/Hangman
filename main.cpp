7ed3e91d1db2d340acbce2114d7dd0990c7119b6
jake-bickle <bickle.jake@gmail.com>
2018-06-02 18:09:51 -0700

    Prototype version of hangman.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 
 int main()
 {
-    Hangman game("Cat");
+    Hangman game("zeBrA");
+    std::cout << "Welcome to hangman v 0.5!\n";
     
-    game.guessLetter('p');
+    char userGuess;
+    while (!(game.isGameWon() || game.isGameLost())){
+        std::cout << "Guess a letter.\n" 
+                  << ">  ";
+        std::cin >> userGuess;
+
+        game.guessLetter(userGuess);
+        std::cout << "\nUnfortunetly, the game's UI is not up and running at the moment. Therefore, I cannot tell you whether that is correct or not.\n";
+    }
+
+    if (game.isGameWon()){
+        std::cout << "Congratulations, you've won the game!\n";
+    } else {
+        std::cout << "Ouch, you've hung the man. Better luck next time.\n";
+    }
     return 0;
 }
 
